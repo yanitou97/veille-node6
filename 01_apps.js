7ace0d3c9e2c3117b9fc5b2c9b1005d8d8f1874a
Yan Nelson-Lapointe <yannl97@hotmail.com>
2018-02-14 10:43:49 -0500

    Fin exercice 5

--- 01_apps.js
+++ 01_apps.js
@@ -14,6 +14,8 @@ app.set('view engine', 'ejs'); // générateur de template
 
 let db // variable qui contiendra le lien sur la BD
 
+
+///////////////////////////////////////////////////// Connexion à mongoDB et au serveur Node.js
 MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
  if (err) return console.log(err)
  db = database.db('carnet_adresse');
@@ -26,11 +28,18 @@ app.listen(8081, () => {
 app.get('/', (req, res) => {
     let cursor = db.collection('adresse')
                 .find().toArray(function(err, resultat){
- 	if (err) return console.log(err)
+ 	if (err) return console.log(err)	
  	console.log(JSON.stringify(resultat))	
  	// transfert du contenu vers la vue index.ejs (renders)
  	// affiche le contenu de la BD
  	res.render('gabarit.ejs', {adresses: resultat}) 
 
- })
+ }) 
+app.post('/ajouter', (req, res) => {
+ 		db.collection('adresse').save(req.body, (err, result) => {
+ 		if (err) return console.log(err)
+ 			console.log('sauvegarder dans la BD')
+ 			res.redirect('/')
+ 		})
+	})                                
 })
